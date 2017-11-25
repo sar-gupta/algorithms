3c2467e22e1b5cd051f85f628d76e884b00e1078
thesemicolonguy <sarthakgupta259@gmail.com>
2017-11-25 20:59:17 +0530

    Update cpp/array-max-subarray-sum

--- c++/array-max-subarray-sum.cpp
+++ c++/array-max-subarray-sum.cpp
@@ -10,25 +10,16 @@ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 For this problem, return the maximum sum.
 */
 
-// KADANE'S ALGORITHM
-
 #include<bits/stdc++.h>
 using namespace std;
 
 int maxSubArray(const vector<int> &A) {
     
-    int max_so_far=INT_MIN, max_ending_here=0;
-    for(int i=0;i<A.size();i++)
+    int max_so_far=A[0], curr_max=A[0];
+    for(int i=1;i<A.size();i++)
     {
-        max_ending_here = max_ending_here+A[i];
-        
-        max_so_far = max(max_so_far, max_ending_here);
-        
-        if(max_ending_here<0)
-        {
-            max_ending_here = 0;
-        }
-        
+        curr_max = max(curr_max+A[i], A[i]);
+        max_so_far = max(max_so_far, curr_max);        
     }
     return max_so_far;
 }
